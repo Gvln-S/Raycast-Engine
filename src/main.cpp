e552ac5dd22e53121b6dd87670ac11da992ba4a4
Gvln-S <santis.paez2414@outlook.com>
2025-05-29 12:52:10 -0500

    fix: :bug: collisions

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,12 @@
 #define P2 PI/2
 #define P3 3*PI/2
 #define DR 0.0174533
+#define map_x 8;
+#define map_y 8;
+#define map_superfice 64;
 
 float player_x, player_y, player_delta_x, player_delta_y, player_angle;
+bool keys[256] = {false};
 
 void draw_player() {
   glColor3f(1, 1, 0);
@@ -26,7 +30,6 @@ void draw_player() {
   glEnd();
 }
 
-int map_x = 8, map_y = 8, map_superfice = 64;
 int map[] = {
   1,1,1,1,1,1,1,1,
   1,0,0,1,0,0,0,1,
@@ -71,7 +74,7 @@ void draw_rays() {
     if(ray_angle==0 || ray_angle==PI){ ray_x = player_x; ray_y = player_y; depth_field= 8;} // looking straight left or right
     while(depth_field<8) {
       map_ray_x = (int)(ray_x)>>6; map_ray_y = (int)(ray_y)>>6; map_ray = map_ray_y*map_x+map_ray_x;
-      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){horizontal_x = ray_x; horizontal_y = ray_y; distance_horizontal = dist(player_x, player_y, horizontal_x, horizontal_y, ray_angle); dof = 8;} // hit wall
+      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){horizontal_x = ray_x; horizontal_y = ray_y; distance_horizontal = dist(player_x, player_y, horizontal_x, horizontal_y, ray_angle); depth_field= 8;} // hit wall
       else{ray_x += x_offset; ray_y += y_offset; depth_field+= 1;} // next wall
     }
 
@@ -84,11 +87,11 @@ void draw_rays() {
     if(ray_angle== 0|| ray_angle==PI){ ray_x = player_x; ray_y = player_y; depth_field= 8;} // looking straight up or down
     while(depth_field<8) {
       map_ray_x = (int)(ray_x)>>6; map_ray_y = (int)(ray_y)>>6; map_ray = map_ray_y*map_x+map_ray_x;
-      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){vertical_x = ray_x; vertical_y = ray_y; distance_vertical = dist(player_x, player_y, vertical_x, vertical_y, ray_angle); dof = 8;} // hit wall
+      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){vertical_x = ray_x; vertical_y = ray_y; distance_vertical = dist(player_x, player_y, vertical_x, vertical_y, ray_angle); depth_field= 8;} // hit wall
       else{ray_x += x_offset; ray_y += y_offset; depth_field += 1;} // next line
     }
-    if(distance_vertical<distance_horizontal){ray_x = vertical_x  ; ray_y = vertical_y  ; distance_total = distance_vertical  ; glColor3f(0.9,0,0); }
-    if(distance_vertical>distance_horizontal){ray_x = horizontal_x; ray_y = horizontal_y; distance_total = distance_horizontal; glColor3f(0.7,0,0); }
+    if(distance_vertical<distance_horizontal){ray_x = vertical_x  ; ray_y = vertical_y  ; distance_total = distance_vertical  ; glColor3f(0.9,0.9,0.9); }
+    if(distance_vertical>distance_horizontal){ray_x = horizontal_x; ray_y = horizontal_y; distance_total = distance_horizontal; glColor3f(0.7,0.7,0.7); }
     glLineWidth(3); glBegin(GL_LINES); glVertex2i(player_x, player_y); glVertex2i(ray_x, ray_y); glEnd();
 
     // draw 3d lines
@@ -101,8 +104,46 @@ void draw_rays() {
   }
 }
 
+void key_down(unsigned char key, int position_x, int position_y) {keys[key] = true;}
+void key_up(unsigned char key, int position_x, int position_y  ) {keys[key] = false;}
+
+float frame_one, frame_two, fps;
+
 void display()
 {
+  frame_two = glutGet(GLUT_ELAPSED_TIME); fps = (frame_two-frame_one); frame_one = glutGet(GLUT_ELAPSED_TIME);
+
+  int x_equalize = 0; if(player_delta_x<0) {x_equalize=-20;} else{x_equalize=20;}
+  int y_equalize = 0; if(player_delta_y<0) {y_equalize=-20;} else{y_equalize=20;}
+  int player_grip_x = player_x/64.0, player_grip_add_x_equilize = (player_x+x_equalize)/64.0, player_grip_sub_x_equilize = (player_x-x_equalize)/64.0;
+  int player_grip_y = player_y/64.0, player_grip_add_y_equilize = (player_y+y_equalize)/64.0, player_grip_sub_y_equilize = (player_y-y_equalize)/64.0;
+
+  if(keys['w']) {
+    if(map[player_grip_y*map_x              + player_grip_add_x_equilize]==0){player_x+=player_delta_x*0.025*fps;}
+    if(map[player_grip_add_y_equilize*map_x + player_grip_x             ]==0){player_y+=player_delta_y*0.025*fps;}
+  }
+  if(keys['s']) {
+    if(map[player_grip_y*map_x              + player_grip_sub_x_equilize]==0){player_x-=player_delta_x*0.025*fps;}
+    if(map[player_grip_sub_y_equilize*map_x + player_grip_x             ]==0){player_y-=player_delta_y*0.025*fps;}
+  }
+  if(keys['a']) {
+    float strafe_x = cos(player_angle+PI/2)*0.1*fps, strafe_y = sin(player_angle+PI/2)*0.1*fps;
+    int map_x_a = (int)((player_x+strafe_x)/64),map_y_a = (int)((player_y+strafe_y)/64);
+
+    if(map[player_grip_y * map_x + map_x_a] == 0) player_x += strafe_x;
+    if(map[map_y_a * map_x + player_grip_x] == 0) player_y += strafe_y;
+  }
+
+  if(keys['d']) {
+    float strafe_x = cos(player_angle-PI/2)*0.1*fps, strafe_y = sin(player_angle-PI/2)*0.1*fps;
+    int map_x_d = (int)((player_x+strafe_x)/64), map_y_d = (int)((player_y+strafe_y)/64);
+
+    if(map[player_grip_y * map_x + map_x_d] == 0) player_x += strafe_x;
+    if(map[map_y_d * map_x + player_grip_x] == 0) player_y += strafe_y;
+  }
+
+
+  glutPostRedisplay();
   glClear(GL_COLOR_BUFFER_BIT);
 
   draw_map_2d();
@@ -112,20 +153,16 @@ void display()
   glutSwapBuffers();
 }
 
-void buttons(unsigned char key, int position_x, int position_y) {
-  if(key == 'a'){player_x += player_delta_y; player_y -= player_delta_x;}
-  if(key == 'd'){player_x -= player_delta_y; player_y += player_delta_x;}
-  if(key == 'w'){player_x += player_delta_x; player_y += player_delta_y;}
-  if(key == 's'){player_x -= player_delta_x; player_y -= player_delta_y;}
-  glutPostRedisplay();
+void resize(int width, int height) {
+  glutReshapeWindow(1024, 512);
 }
 
 void mouse_move(int mouse_x, int mouse_y) {
-  int last_x = 512, delta_x = mouse_x - last_x;
-  float sensitivity = 0.003f;
+  static int last_x = 512; int delta_x = mouse_x - last_x; float sensitivity = 0.005f;
   last_x = mouse_x; player_angle += delta_x * sensitivity;
-  if(player_angle<   0) {player_angle += 2 * PI;}
+  if(player_angle<0) {player_angle += 2 * PI;}
   if(player_angle>2*PI) {player_angle -= 2 * PI;}
+  player_delta_x = cos(player_angle) * 5; player_delta_y = sin(player_angle) * 5;
   glutPostRedisplay();
 }
 
@@ -143,6 +180,7 @@ int main(int argc, char** argv)
 
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
   glutInitWindowSize(1024,512);
+  glutInitWindowPosition(200, 200);
 
   glutCreateWindow("RayCast");
 
@@ -151,8 +189,12 @@ int main(int argc, char** argv)
   // reder loop
   glutDisplayFunc(display);
 
+  // resize
+  glutReshapeFunc(resize);
+
   // keyboard and mouse inputs
-  glutKeyboardFunc(buttons);
+  glutKeyboardFunc(key_down);
+  glutKeyboardUpFunc(key_up);
   glutPassiveMotionFunc(mouse_move);
 
   glutMainLoop();
