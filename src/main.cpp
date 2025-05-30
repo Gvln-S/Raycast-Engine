e541cc6fe11b88263d48a58fc0ca661d7666f543
Gvln-S <santis.paez2414@outlook.com>
2025-05-29 22:12:25 -0500

    feat: walls manual texture + doors function

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <cmath>
+#include <iostream>
+#include <ostream>
 #include <stdlib.h>
 #include <math.h>
 
@@ -177,22 +179,22 @@ void draw_player_2d() {
 }
 
 int map_x = 8, map_y = 8, map_superfice = 64;
-int map[] = {
-  1,1,1,1,1,1,2,1,
+int map_walls[] = {
+  1,1,1,1,1,1,1,1,
   1,0,0,1,0,0,0,1,
+  1,0,0,1,0,0,0,1,
+  1,1,4,1,0,0,0,1,
   1,0,0,0,0,0,0,1,
   1,0,0,0,0,0,0,1,
   1,0,0,0,0,0,0,1,
-  1,1,1,0,0,1,0,1,
-  1,0,0,0,0,0,0,1,
-  1,1,1,1,1,2,1,1,
+  1,1,1,1,1,1,1,1,
 };
 
 void draw_map_2d() {
   int map_x_pos,map_y_pos,map_wall_x,map_wall_y;
   for(map_y_pos = 0; map_y_pos < map_y; map_y_pos++) {
     for(map_x_pos = 0; map_x_pos < map_x; map_x_pos++) {
-      if(map[map_y_pos*map_x+map_x_pos] != 0) { glColor3f(1, 1, 1); } else { glColor3f(0, 0, 0); }
+      if(map_walls[map_y_pos*map_x+map_x_pos]>0) { glColor3f(1, 1, 1); } else { glColor3f(0, 0, 0); }
       map_wall_x = map_x_pos*map_superfice; map_wall_y = map_y_pos*map_superfice;
       glBegin(GL_QUADS);
       glVertex2i(map_wall_x              +1, map_wall_y              +1);
@@ -205,7 +207,7 @@ void draw_map_2d() {
 }
 
 void draw_rays_2d() {
-  glColor3f(0.5,0.5,0.5); glBegin(GL_QUADS); glVertex2i(526,  0); glVertex2i(1006,  0); glVertex2i(1006,160); glVertex2i(526,160); glEnd();	
+  glColor3f(0.1,0.1,0.5); glBegin(GL_QUADS); glVertex2i(526,  0); glVertex2i(1006,  0); glVertex2i(1006,160); glVertex2i(526,160); glEnd();	
   glColor3f(0,0,0); glBegin(GL_QUADS); glVertex2i(526,160); glVertex2i(1006,160); glVertex2i(1006,320); glVertex2i(526,320); glEnd();
 
   int ray, map_ray_x, map_ray_y, map_ray, depth_field, side; 
@@ -225,7 +227,7 @@ void draw_rays_2d() {
 
     while(depth_field<8) {
       map_ray_x = (int)(ray_x)>>6; map_ray_y = (int)(ray_y)>>6; map_ray = map_ray_y*map_x+map_ray_x;
-      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){horizontal_map_texture = map[map_ray]-1; depth_field= 8; distance_vertical = cos(deg_to_rad(ray_angle))*(ray_x - player_x)-sin(deg_to_rad(ray_angle))*(ray_y - player_y);} // hit wall
+      if(map_ray>0 && map_ray<map_x*map_y && map_walls[map_ray]>0){vertical_map_texture = map_walls[map_ray]-1; depth_field= 8; distance_vertical = cos(deg_to_rad(ray_angle))*(ray_x - player_x)-sin(deg_to_rad(ray_angle))*(ray_y - player_y);} // hit wall
       else{ray_x+=x_offset; ray_y+=y_offset; depth_field+=1;} // next line
     }
     vertical_x=ray_x; vertical_y=ray_y;
@@ -239,7 +241,7 @@ void draw_rays_2d() {
 
     while(depth_field<8) {
       map_ray_x = (int)(ray_x)>>6; map_ray_y = (int)(ray_y)>>6; map_ray = map_ray_y*map_x+map_ray_x;
-      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){vertical_map_texture = map[map_ray]-1; depth_field= 8; distance_horizontal=cos(deg_to_rad(ray_angle))*(ray_x - player_x)-sin(deg_to_rad(ray_angle))*(ray_y-player_y);} // hit wall
+      if(map_ray>0 && map_ray<map_x*map_y && map_walls[map_ray]>0){horizontal_map_texture = map_walls[map_ray]-1; depth_field= 8; distance_horizontal=cos(deg_to_rad(ray_angle))*(ray_x - player_x)-sin(deg_to_rad(ray_angle))*(ray_y-player_y);} // hit wall
       else{ray_x += x_offset; ray_y += y_offset; depth_field+=1;} // next wall
     }
 
@@ -259,24 +261,24 @@ void draw_rays_2d() {
     int wall_y;
     float texture_y = texture_y_offset*texture_y_step+horizontal_map_texture*32;
     float texture_x; 
-    if(shade==1) {texture_x = std::fmod(ray_x, 64.0f); if (ray_angle > PI) {texture_x = 64.0f - texture_x;}}
-    else         {texture_x = std::fmod(ray_y, 64.0f); if (ray_angle<PI/2 || ray_angle > 3*PI/2) {texture_x = 64-texture_x;}}
-    texture_x = (int)(texture_x/2.0f)%32;
+    if(shade==1) {texture_x = (int)(ray_x/2.0)%32; if (ray_angle>180) {texture_x = 31-texture_x;}}
+    else         {texture_x = (int)(ray_y/2.0)%32; if (ray_angle>90 && ray_angle<270) {texture_x = 31-texture_x;}}
 
     for(wall_y = 0; wall_y<line_horizontal; wall_y++){
       float c=All_Textures[(int)(texture_y)*32+(int)(texture_x)]*shade;
-      glColor3f(c, c, c);
+      if(horizontal_map_texture==0) {glColor3f(c,     c/2.0, c/2.0);}
+      if(horizontal_map_texture==1) {glColor3f(c,     c    , c/2.0);}
+      if(horizontal_map_texture==2) {glColor3f(c/2.0, c/2.0, c    );}
+      if(horizontal_map_texture==3) {glColor3f(c/2.0, c    , c/2.0);}
       glLineWidth(8); glBegin(GL_POINTS); glVertex2i(ray*8+530,wall_y+line_offset); glEnd();
       texture_y+=texture_y_step;
     }
-    ray_angle += DR; if(ray_angle<0){ray_angle+=2*PI;} if(ray_angle>2*PI){ray_angle -= 2*PI;}
-
+    ray_angle = fix_angle(ray_angle-1);
   }
 }
 
 void key_down(unsigned char key, int position_x, int position_y) {keys[key] = true;}
 void key_up(unsigned char key, int position_x, int position_y  ) {keys[key] = false;}
-
 
 void display()
 {
@@ -288,28 +290,37 @@ void display()
   int player_grip_y = player_y/64.0, player_grip_add_y_equilize = (player_y+y_equalize)/64.0, player_grip_sub_y_equilize = (player_y-y_equalize)/64.0;
 
   if(keys['w']) {
-    if(map[player_grip_y*map_x              + player_grip_add_x_equilize]==0){player_x+=player_delta_x*0.025*fps;}
-    if(map[player_grip_add_y_equilize*map_x + player_grip_x             ]==0){player_y+=player_delta_y*0.025*fps;}
+    if(map_walls[player_grip_y*map_x              + player_grip_add_x_equilize]==0){player_x+=player_delta_x*0.025*fps;}
+    if(map_walls[player_grip_add_y_equilize*map_x + player_grip_x             ]==0){player_y+=player_delta_y*0.025*fps;}
   }
   if(keys['s']) {
-    if(map[player_grip_y*map_x              + player_grip_sub_x_equilize]==0){player_x-=player_delta_x*0.025*fps;}
-    if(map[player_grip_sub_y_equilize*map_x + player_grip_x             ]==0){player_y-=player_delta_y*0.025*fps;}
+    if(map_walls[player_grip_y*map_x              + player_grip_sub_x_equilize]==0){player_x-=player_delta_x*0.025*fps;}
+    if(map_walls[player_grip_sub_y_equilize*map_x + player_grip_x             ]==0){player_y-=player_delta_y*0.025*fps;}
   }
   if(keys['a']) {
-    float strafe_x = cos(player_angle-PI/2)*0.1*fps, strafe_y = sin(player_angle-PI/2)*0.1*fps;
-    int map_x_a = (int)((player_x+strafe_x)/64),map_y_a = (int)((player_y+strafe_y)/64);
+    float angle = fix_angle(player_angle - 90);
+    float strafe_x =  cos(deg_to_rad(angle));
+    float strafe_y = -sin(deg_to_rad(angle));
 
-    if(map[player_grip_y * map_x + map_x_a] == 0) player_x += strafe_x;
-    if(map[map_y_a * map_x + player_grip_x] == 0) player_y += strafe_y;
+    if(map_walls[player_grip_y * map_x + player_grip_add_x_equilize] == 0) {player_x += strafe_x * 0.08f * fps;}
+    if(map_walls[player_grip_add_y_equilize * map_x + player_grip_x] == 0) {player_y += strafe_y * 0.08f * fps;}
   }
   if(keys['d']) {
-    float strafe_x = cos(player_angle+PI/2)*0.1*fps, strafe_y = sin(player_angle+PI/2)*0.1*fps;
-    int map_x_d = (int)((player_x+strafe_x)/64), map_y_d = (int)((player_y+strafe_y)/64);
+    float angle = fix_angle(player_angle + 90);
+    float strafe_x =  cos(deg_to_rad(angle));
+    float strafe_y = -sin(deg_to_rad(angle));
 
-    if(map[player_grip_y * map_x + map_x_d] == 0) player_x += strafe_x;
-    if(map[map_y_d * map_x + player_grip_x] == 0) player_y += strafe_y;
+    if(map_walls[player_grip_y * map_x + player_grip_sub_x_equilize] == 0) {player_x += strafe_x * 0.08f * fps;}
+    if(map_walls[player_grip_sub_y_equilize * map_x + player_grip_x] == 0) {player_y += strafe_y * 0.08f * fps;}
   }
-
+  if(keys['e']) {
+    int x_equalize = 0; if(player_delta_x<0) {x_equalize=-25;} else{x_equalize=25;}
+    int y_equalize = 0; if(player_delta_y<0) {y_equalize=-25;} else{y_equalize=25;} 
+    int player_grip_x = player_x/64.0, player_grip_add_x_equilize = (player_x+x_equalize)/64.0;
+    int player_grip_y = player_y/64.0, player_grip_add_y_equilize = (player_y+y_equalize)/64.0;
+    if(map_walls[player_grip_add_y_equilize*map_x+player_grip_add_x_equilize]==4) {map_walls[player_grip_add_y_equilize*map_x+player_grip_add_x_equilize]=0;}
+    
+  }
 
   glutPostRedisplay();
   glClear(GL_COLOR_BUFFER_BIT);
@@ -326,17 +337,23 @@ void resize(int width, int height) {
 }
 
 void mouse_move(int mouse_x, int mouse_y) {
-  static int last_x = 512; int delta_x = mouse_x - last_x; float sensitivity = 0.005f;
-  last_x = mouse_x; player_angle += delta_x * sensitivity;
-  if(player_angle<0) {player_angle += 2 * PI;}
-  if(player_angle>2*PI) {player_angle -= 2 * PI;}
-  player_delta_x = cos(player_angle) * 5; player_delta_y = sin(player_angle) * 5;
+  static int last_x = 512;
+  int delta_x = mouse_x - last_x;
+  float sensitivity = 0.2f;
+
+  last_x = mouse_x;
+
+  player_angle += delta_x * sensitivity;
+  player_angle = fix_angle((int)player_angle);
+
+  player_delta_x =  cos(deg_to_rad(player_angle))*5;
+  player_delta_y = -sin(deg_to_rad(player_angle))*5; 
+
   glutPostRedisplay();
 }
 
 void init() {
   gluOrtho2D(0,1024,512,0);
-
   player_x = 300; player_y = 300; player_delta_x = cos(player_angle)*5; player_delta_y = sin(player_angle)*5;
 }
 
