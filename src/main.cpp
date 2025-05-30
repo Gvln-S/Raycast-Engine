3d07628b6a48afa9a3b882aa9d57a248fab84aa6
Gvln-S <santis.paez2414@outlook.com>
2025-05-29 20:26:28 -0500

    fix: :bug: change rays structure

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@
 #include <math.h>
 
 #define PI 3.1415926535
-#define P2 PI/2
-#define P3 3*PI/2
-#define DR 0.0174533
 
+float deg_to_rad(int a) {return a*PI/180;}
+int fix_angle(int a) {if(a>329) {a-=360;} if(a<0) {a+=360;} return a;}
+float distance(float ax, float ay, float bx, float by, float angle) {return cos(deg_to_rad(angle))*(bx-ax)-sin(deg_to_rad(angle))*(by-ay);}
 float player_x, player_y, player_delta_x, player_delta_y, player_angle;
+float frame_one, frame_two, fps;
 bool keys[256] = {false};
 
 int All_Textures[]=
@@ -161,7 +162,7 @@ int All_Textures[]=
   0,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,0,         
 };
 
-void draw_player() {
+void draw_player_2d() {
   glColor3f(1, 1, 0);
   glPointSize(8);
   glBegin(GL_POINTS);
@@ -177,21 +178,21 @@ void draw_player() {
 
 int map_x = 8, map_y = 8, map_superfice = 64;
 int map[] = {
-  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,2,1,
   1,0,0,1,0,0,0,1,
   1,0,0,0,0,0,0,1,
   1,0,0,0,0,0,0,1,
   1,0,0,0,0,0,0,1,
   1,1,1,0,0,1,0,1,
   1,0,0,0,0,0,0,1,
-  1,1,1,1,1,1,1,1,
+  1,1,1,1,1,2,1,1,
 };
 
 void draw_map_2d() {
   int map_x_pos,map_y_pos,map_wall_x,map_wall_y;
   for(map_y_pos = 0; map_y_pos < map_y; map_y_pos++) {
     for(map_x_pos = 0; map_x_pos < map_x; map_x_pos++) {
-      if(map[map_y_pos*map_x+map_x_pos] == 1) { glColor3f(1, 1, 1); } else { glColor3f(0, 0, 0); }
+      if(map[map_y_pos*map_x+map_x_pos] != 0) { glColor3f(1, 1, 1); } else { glColor3f(0, 0, 0); }
       map_wall_x = map_x_pos*map_superfice; map_wall_y = map_y_pos*map_superfice;
       glBegin(GL_QUADS);
       glVertex2i(map_wall_x              +1, map_wall_y              +1);
@@ -203,56 +204,64 @@ void draw_map_2d() {
   }
 }
 
-float dist(float ax, float ay, float bx, float by, float ang) {
-  return ( sqrt((bx-ax)*(bx-ax) + (by-ay)*(by-ay)) );
-}
+void draw_rays_2d() {
+  glColor3f(0.5,0.5,0.5); glBegin(GL_QUADS); glVertex2i(526,  0); glVertex2i(1006,  0); glVertex2i(1006,160); glVertex2i(526,160); glEnd();	
+  glColor3f(0,0,0); glBegin(GL_QUADS); glVertex2i(526,160); glVertex2i(1006,160); glVertex2i(1006,320); glVertex2i(526,320); glEnd();
 
-void draw_rays() {
-  int ray, map_ray_x, map_ray_y, map_ray, depth_field; float ray_x, ray_y, ray_angle, x_offset, y_offset, distance_total;
-  ray_angle = player_angle - DR*30; if(ray_angle<0){ray_angle+=2*PI;} if(ray_angle>2*PI){ray_angle -= 2*PI;}
+  int ray, map_ray_x, map_ray_y, map_ray, depth_field, side; 
+  float vertical_x, vertical_y, ray_x, ray_y, ray_angle, x_offset, y_offset, distance_horizontal, distance_vertical;
+
+  ray_angle = fix_angle(player_angle+30);
+
   for(ray=0; ray<60; ray++) {
-    // check horizontal lines
-    depth_field = 0;
-    float distance_horizontal= 100000000, horizontal_x = player_x, horizontal_y = player_y;
-    float aTan = -1/tan(ray_angle);
-    if(ray_angle>PI){ray_y = (((int)player_y>>6)<<6)-0.0001; ray_x = (player_y - ray_y)*aTan+player_x; y_offset =-64; x_offset =-y_offset*aTan;} // looking up
-    if(ray_angle<PI){ray_y = (((int)player_y>>6)<<6)+64;     ray_x = (player_y - ray_y)*aTan+player_x; y_offset = 64; x_offset =-y_offset*aTan;} // looking down
-    if(ray_angle==0 || ray_angle==PI){ ray_x = player_x; ray_y = player_y; depth_field= 8;} // looking straight left or right
-    while(depth_field<8) {
-      map_ray_x = (int)(ray_x)>>6; map_ray_y = (int)(ray_y)>>6; map_ray = map_ray_y*map_x+map_ray_x;
-      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){horizontal_x = ray_x; horizontal_y = ray_y; distance_horizontal = dist(player_x, player_y, horizontal_x, horizontal_y, ray_angle); depth_field= 8;} // hit wall
-      else{ray_x += x_offset; ray_y += y_offset; depth_field+= 1;} // next wall
-    }
+    int vertical_map_texture = 0, horizontal_map_texture = 0; // map texture number
 
-    // check vertical lines
-    depth_field= 0;
-    float distance_vertical = 100000000, vertical_x = player_x, vertical_y = player_y;
-    float nTan = -tan(ray_angle);
-    if(ray_angle>P2 && ray_angle<P3){ray_x = (((int)player_x>>6)<<6)-0.0001; ray_y = (player_x - ray_x)*nTan+player_y; x_offset =-64; y_offset =-x_offset*nTan;} // looking left
-    else{ray_x = (((int)player_x>>6)<<6)+64;     ray_y = (player_x - ray_x)*nTan+player_y; x_offset = 64; y_offset =-x_offset*nTan;} // looking rigth
-    if(ray_angle== 0|| ray_angle==PI){ ray_x = player_x; ray_y = player_y; depth_field= 8;} // looking straight up or down
+    // vertical lines
+    depth_field = 0; side = 0; distance_vertical = 100000000;
+    float Tan = tan(deg_to_rad(ray_angle));
+         if(cos(deg_to_rad(ray_angle))> 0.001) {ray_x = (((int)player_x>>6)<<6)+64;    ray_y = (player_x - ray_x)*Tan+player_y; x_offset = 64; y_offset =-x_offset*Tan;} // looking left
+    else if(cos(deg_to_rad(ray_angle))<-0.001) {ray_x = (((int)player_x>>6)<<6)-0.001; ray_y = (player_x - ray_x)*Tan+player_y; x_offset =-64; y_offset =-x_offset*Tan;} // looking rigth
+    else {ray_x=player_x; ray_y=player_y; depth_field=8;}
+
     while(depth_field<8) {
       map_ray_x = (int)(ray_x)>>6; map_ray_y = (int)(ray_y)>>6; map_ray = map_ray_y*map_x+map_ray_x;
-      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){vertical_x = ray_x; vertical_y = ray_y; distance_vertical = dist(player_x, player_y, vertical_x, vertical_y, ray_angle); depth_field= 8;} // hit wall
-      else{ray_x += x_offset; ray_y += y_offset; depth_field += 1;} // next line
+      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){horizontal_map_texture = map[map_ray]-1; depth_field= 8; distance_vertical = cos(deg_to_rad(ray_angle))*(ray_x - player_x)-sin(deg_to_rad(ray_angle))*(ray_y - player_y);} // hit wall
+      else{ray_x+=x_offset; ray_y+=y_offset; depth_field+=1;} // next line
+    }
+    vertical_x=ray_x; vertical_y=ray_y;
+
+    // horizontal lines
+    depth_field = 0, distance_horizontal= 100000000;
+    Tan = 1.0/Tan;
+         if(sin(deg_to_rad(ray_angle))> 0.001) {ray_y = (((int)player_y>>6)<<6)-0.001; ray_x = (player_y - ray_y)*Tan+player_x; y_offset =-64; x_offset =-y_offset*Tan;} // looking up
+    else if(sin(deg_to_rad(ray_angle))<-0.001) {ray_y = (((int)player_y>>6)<<6)+64;    ray_x = (player_y - ray_y)*Tan+player_x; y_offset = 64; x_offset =-y_offset*Tan;} // looking down
+    else {ray_x=player_x; ray_y=player_y; depth_field=8;}
+
+    while(depth_field<8) {
+      map_ray_x = (int)(ray_x)>>6; map_ray_y = (int)(ray_y)>>6; map_ray = map_ray_y*map_x+map_ray_x;
+      if(map_ray>0 && map_ray<map_x*map_y && map[map_ray] == 1){vertical_map_texture = map[map_ray]-1; depth_field= 8; distance_horizontal=cos(deg_to_rad(ray_angle))*(ray_x - player_x)-sin(deg_to_rad(ray_angle))*(ray_y-player_y);} // hit wall
+      else{ray_x += x_offset; ray_y += y_offset; depth_field+=1;} // next wall
     }
 
     float shade=1;
-    if(distance_vertical<distance_horizontal){shade = 0.5; ray_x = vertical_x  ; ray_y = vertical_y  ; distance_total = distance_vertical  ; glColor3f(0.9,0.9,0.9); }
-    if(distance_vertical>distance_horizontal){ray_x = horizontal_x; ray_y = horizontal_y; distance_total = distance_horizontal; glColor3f(0.7,0.7,0.7); }
-    glLineWidth(3); glBegin(GL_LINES); glVertex2i(player_x, player_y); glVertex2i(ray_x, ray_y); glEnd();
+    glColor3f(0.8,0.8,0.8);
+    if(distance_vertical<distance_horizontal){horizontal_map_texture = vertical_map_texture; shade = 0.5; ray_x = vertical_x; ray_y = vertical_y; distance_horizontal = distance_vertical; glColor3f(0.6,0.6,0.6);}
+    glLineWidth(2); glBegin(GL_LINES); glVertex2i(player_x, player_y); glVertex2i(ray_x, ray_y); glEnd();
 
     // draw 3d lines
-    float pa_ra_d = player_angle - ray_angle; if(pa_ra_d<0){pa_ra_d+=2*PI;} if(pa_ra_d>2+PI){pa_ra_d-=2*PI;} distance_total=distance_total*cos(pa_ra_d); // fix fish eye
-    float line_horizontal = ( map_superfice *320)/distance_total;
+    int ca=fix_angle(player_angle-ray_angle); distance_horizontal=distance_horizontal*cos(deg_to_rad(ca));
+    int line_horizontal = (map_superfice*320)/(distance_horizontal); if(line_horizontal>320) {line_horizontal=320;}
     float texture_y_step = 32.0/(float)line_horizontal;
     float texture_y_offset = 0;
     if(line_horizontal>320){texture_y_offset = (line_horizontal-320)/2.0; line_horizontal=320;}   // line height and limit
-    float line_offset = 160 - line_horizontal/2;                                                                                                         // line offset
-                                                                                                                                                         //
+    float line_offset = 160 - (line_horizontal>>1);                                               // line offset
+                                                                                                                                                            
     int wall_y;
-    float texture_y = texture_y_offset*texture_y_step;
-    float texture_x = (int)(ray_x/2.0)%32;
+    float texture_y = texture_y_offset*texture_y_step+horizontal_map_texture*32;
+    float texture_x; 
+    if(shade==1) {texture_x = std::fmod(ray_x, 64.0f); if (ray_angle > PI) {texture_x = 64.0f - texture_x;}}
+    else         {texture_x = std::fmod(ray_y, 64.0f); if (ray_angle<PI/2 || ray_angle > 3*PI/2) {texture_x = 64-texture_x;}}
+    texture_x = (int)(texture_x/2.0f)%32;
 
     for(wall_y = 0; wall_y<line_horizontal; wall_y++){
       float c=All_Textures[(int)(texture_y)*32+(int)(texture_x)]*shade;
@@ -260,7 +269,6 @@ void draw_rays() {
       glLineWidth(8); glBegin(GL_POINTS); glVertex2i(ray*8+530,wall_y+line_offset); glEnd();
       texture_y+=texture_y_step;
     }
-                                                                                                                                                         //
     ray_angle += DR; if(ray_angle<0){ray_angle+=2*PI;} if(ray_angle>2*PI){ray_angle -= 2*PI;}
 
   }
@@ -269,7 +277,6 @@ void draw_rays() {
 void key_down(unsigned char key, int position_x, int position_y) {keys[key] = true;}
 void key_up(unsigned char key, int position_x, int position_y  ) {keys[key] = false;}
 
-float frame_one, frame_two, fps;
 
 void display()
 {
@@ -308,8 +315,8 @@ void display()
   glClear(GL_COLOR_BUFFER_BIT);
 
   draw_map_2d();
-  draw_rays();
-  draw_player();
+  draw_rays_2d();
+  draw_player_2d();
 
   glutSwapBuffers();
 }
@@ -327,9 +334,7 @@ void mouse_move(int mouse_x, int mouse_y) {
   glutPostRedisplay();
 }
 
-
 void init() {
-  glClearColor(0.1, 0.1, 0.1, 0);
   gluOrtho2D(0,1024,512,0);
 
   player_x = 300; player_y = 300; player_delta_x = cos(player_angle)*5; player_delta_y = sin(player_angle)*5;
